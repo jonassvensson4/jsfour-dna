ee9385278a53aeb7412a5509f0576bf2924a5379
Jonas Svensson <jonassvensson4@hotmail.com>
2018-07-21 21:00:00 +0200

    Update jsfour_dna.sql

--- jsfour_dna.sql
+++ jsfour_dna.sql
@@ -1,6 +1,6 @@
 USE `essentialmode`;
 
-CREATE TABLE `jsfour_dnaTEMP` (
+CREATE TABLE `jsfour_dna` (
   `pk` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `killer` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   `dead` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
@@ -8,6 +8,5 @@ CREATE TABLE `jsfour_dnaTEMP` (
   `type` varchar(255) COLLATE utf8mb4_bin DEFAULT 'murder',
   `lastdigits` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   `uploader` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `datum` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`pk`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `datum` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL
+);
