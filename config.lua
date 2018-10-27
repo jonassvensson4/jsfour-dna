81ac5c9e95a41e10f2405701fbf36b98b3cd4071
Jonas Svensson <jonassvensson4@hotmail.com>
2018-10-27 15:23:12 +0200

    English translation

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@ Config.Computer = {
 		Pos   = {x = 459.7448425293, y = -988.79364013672, z = 23.984875030518},
 		Size  = {x = 2.0, y = 2.0, z = 1.0},
 		Color = {r = 26, g = 55, b = 186},
-		Hint = 'Tryck på ~INPUT_PICKUP~ för att använda DNA-databasen',
+		Hint = 'Press ~INPUT_PICKUP~ to use the DNA-database',
 		Type = 27,
 }
